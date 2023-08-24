de93710ee561663a4f611f6dee3766dac2ba578a
kenny7030 <kariukikennedy6@gmail.com>
2023-08-24 15:20:23 -0700

    nop.c

---
+++ 0x19.C-Stacks,Queues_LIFO,FIFO/nop.c
@@ -0,0 +1,9 @@
+#include "monty.h"
+
+/* Function to perform a no-operation (nop) */
+void f_nop(stack_t **head, __attribute__((unused)) unsigned int line_number)
+{
+    (void)head; // Unused parameter
+    /* This opcode does nothing */
+}
+
